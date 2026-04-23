9e81c70202bc0641679ced4c1a703b632946ad26
JustSomeGuy <19639799+JustSomeGuy1234@users.noreply.github.com>
2026-04-23 06:27:39 +0100

    Updated address and expected bytes for 1.04.00

--- dllmain.cpp
+++ dllmain.cpp
@@ -12,14 +12,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
 	case DLL_PROCESS_ATTACH: {
-		HMODULE hExecutable = GetModuleHandle(NULL);
 		bool failed = false;
 		try {
 			failed = failed || !PatchManeuver();
-			/*failed = failed || !PatchAxiomRange();
-			log << "AxiomRange patch: " << (failed ? "Failed" : "Success") << std::endl;
-			failed = failed || !PatchAxiomSpeed();
-			log << "AxiomSpeed patch: " << (failed ? "Failed" : "Success") << std::endl;*/
+			//failed = failed || !PatchAxiomRange();
+			//log << "AxiomRange patch: " << (failed ? "Failed" : "Success") << std::endl;
+			//failed = failed || !PatchAxiomSpeed();
+			//log << "AxiomSpeed patch: " << (failed ? "Failed" : "Success") << std::endl;
 		} catch(...) {
 			failed = true;
 		}
@@ -39,9 +38,10 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 }
 
 bool PatchManeuver() {
-	char* pCall = (char*)0x140e25e5d;
+	//char* pCall = (char*)0x140e25e5d;
+	char* pCall = (char*)0x140EA5B0E;
 	char nops[5] = { 0x90, 0x90, 0x90, 0x90, 0x90 };
-	char expectedBytes[5] = { 0xe8, 0x6e, 0xce, 0xff, 0xff };
+	char expectedBytes[5] = { 0xE8, 0x4D, 0xD0, 0xFF, 0xFF };
 
 	if (memcmp(pCall, expectedBytes, 5) != 0)
 		return false;
@@ -54,7 +54,9 @@ bool PatchManeuver() {
 	return true;
 }
 
-/*bool PatchAxiomRange() {
+/*
+* these are out of date
+bool PatchAxiomRange() {
 	float* pRange = (float*)0x146e22038;
 	if (*(UINT32*)pRange != 1101004800)
 		return false;
@@ -68,18 +70,24 @@ bool PatchManeuver() {
 }
 
 bool PatchAxiomSpeed() {
-	char* pReelDuration = (char*)0x14042074A;
-	char* pPropellDuration = (char*)0x1404209AB;
-	char zeroDurationInstr[9] = { 0xc6, 0x84, 0x3e, 0xe7, 0x09, 0x00, 0x00, 0x00, 0x90};
+	char* pReelDuration = (char*)0x140420705;
+	char zeroReelDurationJmp[2] = { 0xeb, 0x43 };
+	char reelExpected[2] = { 0x76, 0x43 };
 
-	char expected[9] = { 0xF3, 0x0F, 0x11, 0xBC, 0x3E, 0xE4, 0x09, 0x00, 0x00 };
-	if (memcmp(pReelDuration, expected, 9) != 0)
+	char* pPropellDuration = (char*)0x1404209a0;
+	char zeroPropellJmp[2] = { 0xeb, 0x06};
+	char propellExpected[2] = { 0x76, 0x06 };
+
+	if (memcmp(pReelDuration, reelExpected, 2) != 0)
 		return false;
+	if (memcmp(pPropellDuration, propellExpected, 2) != 0)
+		return false;
+
 
 	DWORD oldprotect;
 	VirtualProtect(pReelDuration, 1024, PAGE_EXECUTE_READWRITE, &oldprotect);
-	memcpy(pReelDuration, zeroDurationInstr, 9);
-	memcpy(pPropellDuration, zeroDurationInstr, 9);
+	memcpy(pReelDuration, zeroReelDurationJmp, 2);
+	memcpy(pPropellDuration, zeroPropellJmp, 2);
 	VirtualProtect(pReelDuration, 1024, oldprotect, &oldprotect);
 
 	return true;
